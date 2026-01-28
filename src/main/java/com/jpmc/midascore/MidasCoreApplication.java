da1b0e9038938fe3885356a97b8398e80156b1ba
MonishaVenkatesh <monisha.v2706@gmail.com>
2026-01-28 12:23:52 +0530

    Made changes in midascoreapplication

--- src/main/java/com/jpmc/midascore/MidasCoreApplication.java
+++ src/main/java/com/jpmc/midascore/MidasCoreApplication.java
@@ -11,11 +11,4 @@ public class MidasCoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(MidasCoreApplication.class, args);
     }
-    
-    @Bean
-    public RestTemplate restTemplate()
-    {
-    	return new RestTemplate();
-    }
-
 }
