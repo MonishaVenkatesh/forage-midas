12edb4f9e5d692f56ebd03b8183979d2d34842bf
MonishaVenkatesh <monisha.v2706@gmail.com>
2026-01-28 14:25:38 +0530

    added the required for controller

--- src/main/java/com/jpmc/midascore/controller/BalanceController.java
+++ src/main/java/com/jpmc/midascore/controller/BalanceController.java
@@ -1,5 +1,30 @@
 package com.jpmc.midascore.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Balance;
+import com.jpmc.midascore.repository.UserRepository;
+
 public class BalanceController {
+	private final UserRepository userRepository;
+	
+	public BalanceController(UserRepository userRepository)
+	{
+		this.userRepository = userRepository;
+	}
+	
+	@GetMapping("/balance")
+	public Balance getBalance(@RequestParam Long userId)
+	{
+		UserRecord user = userRepository.findById(userId).orElse(null);
+		
+		if(user == null)
+		{
+			return new Balance(0);
+		}
+		return new Balance(user.getBalance());
+	}
 
 }
